e960cf7f7e301cbfdb8e7bbbb0a37766ab779a06
Draganspin <124641854+Draganspin@users.noreply.github.com>
2023-02-08 15:44:40 -0500

    First commit

---
+++ app-files/data.js
@@ -0,0 +1,633 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-atelier-1",
+      "name": "atelier 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3233654856313315,
+          "pitch": 0.4284532270728203,
+          "rotation": 0,
+          "target": "1-atelier-2"
+        },
+        {
+          "yaw": 2.2846359270264394,
+          "pitch": 0.7099570150584817,
+          "rotation": 0,
+          "target": "5-coulloir-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-atelier-2",
+      "name": "atelier 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4942584647926918,
+          "pitch": 0.4932387835972296,
+          "rotation": 0,
+          "target": "0-atelier-1"
+        },
+        {
+          "yaw": 2.8946145947541115,
+          "pitch": 0.26299243184857346,
+          "rotation": 0.7853981633974483,
+          "target": "5-coulloir-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-central",
+      "name": "central",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.613991302059155,
+          "pitch": 0.6744819364048134,
+          "rotation": 0,
+          "target": "6-cuisine"
+        },
+        {
+          "yaw": -1.005273890403906,
+          "pitch": 0.5546807883638945,
+          "rotation": 0,
+          "target": "8-entre-principal"
+        },
+        {
+          "yaw": -2.407230833513596,
+          "pitch": 0.6124383392787216,
+          "rotation": 0,
+          "target": "10-salon"
+        },
+        {
+          "yaw": 2.4811927776959593,
+          "pitch": 0.6381057391124134,
+          "rotation": 0,
+          "target": "4-coulloir-1"
+        },
+        {
+          "yaw": 2.422956494794583,
+          "pitch": 0.3919200610851856,
+          "rotation": 0,
+          "target": "5-coulloir-2"
+        },
+        {
+          "yaw": 1.752669584885596,
+          "pitch": 0.6743877534829608,
+          "rotation": 0,
+          "target": "7-entre-arrire"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-chambre",
+      "name": "chambre",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8952849877321007,
+          "pitch": 0.4867082757123562,
+          "rotation": 0,
+          "target": "5-coulloir-2"
+        },
+        {
+          "yaw": -0.874352519974348,
+          "pitch": 0.32600024709194386,
+          "rotation": 0,
+          "target": "4-coulloir-1"
+        },
+        {
+          "yaw": -0.8525379811326559,
+          "pitch": 0.08727056888390905,
+          "rotation": 0,
+          "target": "8-entre-principal"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-coulloir-1",
+      "name": "coulloir 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03138804813140439,
+          "pitch": 0.27593557546721037,
+          "rotation": 0,
+          "target": "8-entre-principal"
+        },
+        {
+          "yaw": 0.5823983521507046,
+          "pitch": 0.4350448007945289,
+          "rotation": 0.7853981633974483,
+          "target": "6-cuisine"
+        },
+        {
+          "yaw": 0.10150235920863437,
+          "pitch": 0.5862401408368569,
+          "rotation": 0,
+          "target": "2-central"
+        },
+        {
+          "yaw": -0.6074748880126535,
+          "pitch": 0.4640332464896968,
+          "rotation": 5.497787143782138,
+          "target": "10-salon"
+        },
+        {
+          "yaw": 1.0552046983039407,
+          "pitch": 0.6320677295235662,
+          "rotation": 0.7853981633974483,
+          "target": "7-entre-arrire"
+        },
+        {
+          "yaw": 2.3076394082221814,
+          "pitch": 0.6290627863198548,
+          "rotation": 0,
+          "target": "9-salle-de-bain"
+        },
+        {
+          "yaw": 3.108678256324507,
+          "pitch": 0.34662385401461293,
+          "rotation": 0,
+          "target": "3-chambre"
+        },
+        {
+          "yaw": -3.0407352515704282,
+          "pitch": 0.8436199164133029,
+          "rotation": 0,
+          "target": "5-coulloir-2"
+        },
+        {
+          "yaw": -2.6970912451057494,
+          "pitch": 0.5054818147805502,
+          "rotation": 0.7853981633974483,
+          "target": "0-atelier-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-coulloir-2",
+      "name": "coulloir 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7919008573904609,
+          "pitch": 0.5536220745131022,
+          "rotation": 0,
+          "target": "3-chambre"
+        },
+        {
+          "yaw": 2.2953410565025507,
+          "pitch": 0.5216119442196891,
+          "rotation": 0,
+          "target": "0-atelier-1"
+        },
+        {
+          "yaw": -2.2940088300023582,
+          "pitch": 0.7896677124065903,
+          "rotation": 0,
+          "target": "4-coulloir-1"
+        },
+        {
+          "yaw": -2.220572353847647,
+          "pitch": 0.3751254844409164,
+          "rotation": 0,
+          "target": "2-central"
+        },
+        {
+          "yaw": -2.3235055470026253,
+          "pitch": 0.1525835009616472,
+          "rotation": 0,
+          "target": "8-entre-principal"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-cuisine",
+      "name": "cuisine",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.570820819005645,
+          "pitch": 0.5944614040424785,
+          "rotation": 0,
+          "target": "2-central"
+        },
+        {
+          "yaw": -2.478822794959573,
+          "pitch": 0.20138160838514807,
+          "rotation": 0,
+          "target": "10-salon"
+        },
+        {
+          "yaw": -1.9624455712043734,
+          "pitch": 0.4042754858492579,
+          "rotation": 0.7853981633974483,
+          "target": "8-entre-principal"
+        },
+        {
+          "yaw": 3.0867357031891824,
+          "pitch": 0.38752804709367794,
+          "rotation": 5.497787143782138,
+          "target": "4-coulloir-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-entre-arrire",
+      "name": "entrée arrière",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.998015556747081,
+          "pitch": 0.4989328117248579,
+          "rotation": 5.497787143782138,
+          "target": "4-coulloir-1"
+        },
+        {
+          "yaw": -0.6148067172218461,
+          "pitch": 0.1504149326397588,
+          "rotation": 0.7853981633974483,
+          "target": "10-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-entre-principal",
+      "name": "entrée principal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4295422781384595,
+          "pitch": 0.47531768770258154,
+          "rotation": 0,
+          "target": "2-central"
+        },
+        {
+          "yaw": -2.8055947681630435,
+          "pitch": 0.43340946876718434,
+          "rotation": 5.497787143782138,
+          "target": "6-cuisine"
+        },
+        {
+          "yaw": -1.8345426517505636,
+          "pitch": 0.41491372550509986,
+          "rotation": 0.7853981633974483,
+          "target": "10-salon"
+        },
+        {
+          "yaw": -2.5435269677517773,
+          "pitch": 0.22513172241292168,
+          "rotation": 5.497787143782138,
+          "target": "7-entre-arrire"
+        },
+        {
+          "yaw": -2.239514539836808,
+          "pitch": 0.14584393048520994,
+          "rotation": 0,
+          "target": "3-chambre"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-salle-de-bain",
+      "name": "salle de bain",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9550974641607137,
+          "pitch": 0.7497176412315678,
+          "rotation": 0.7853981633974483,
+          "target": "4-coulloir-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-salon",
+      "name": "salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3970758073577407,
+          "pitch": 0.45492922938625036,
+          "rotation": 5.497787143782138,
+          "target": "8-entre-principal"
+        },
+        {
+          "yaw": 2.3247593464750045,
+          "pitch": 0.35360125794058206,
+          "rotation": 0,
+          "target": "6-cuisine"
+        },
+        {
+          "yaw": 2.448367673605694,
+          "pitch": 0.5250103564118547,
+          "rotation": 0,
+          "target": "2-central"
+        },
+        {
+          "yaw": 2.797159460718751,
+          "pitch": 0.32182421252206694,
+          "rotation": 0,
+          "target": "7-entre-arrire"
+        },
+        {
+          "yaw": -2.9236814747893956,
+          "pitch": 0.5906916095992418,
+          "rotation": 0.7853981633974483,
+          "target": "4-coulloir-1"
+        },
+        {
+          "yaw": -2.9434302433054675,
+          "pitch": 0.1523089999278504,
+          "rotation": 0,
+          "target": "9-salle-de-bain"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
